26622b8ed27a408cbff28f53babc9d27df8423c1
pfcoperez <contact@pablofranciscoperez.info>
2018-11-07 11:00:01 +0100

    Moved custom (strict deserialization) semiauto decoder deriver method to a separate package.

--- src/main/scala/org/pfcoperez/Models.scala
+++ src/main/scala/org/pfcoperez/Models.scala
@@ -2,12 +2,9 @@ package org.pfcoperez
 
 import io.circe.{Decoder, Encoder, HCursor}
 import io.circe.generic.extras.Configuration
-import io.circe.generic.extras.decoding.ConfiguredDecoder
-import io.circe.generic.extras.semiauto.{deriveEncoder, deriveDecoder => origDeriveDecoder}
+import io.circe.generic.extras.semiauto.deriveEncoder
+import custom.circe.semiauto.deriveDecoder
 import org.pfcoperez.containers.Sensitive
-import shapeless._
-import shapeless.ops.hlist
-import shapeless.ops.record._
 
 object Models {
 
@@ -33,32 +30,6 @@ object Models {
     implicit lazy val userEncoder: Encoder[UserDetails] = deriveEncoder[UserDetails]
     implicit lazy val contractEncoder: Encoder[Contract] = deriveEncoder[Contract]
 
-    def deriveDecoder[T, Repr <: HList, KeysRepr <: HList](
-      implicit
-      lgen: LabelledGeneric.Aux[T, Repr],
-      keys: Keys.Aux[Repr, KeysRepr],
-      toTraversable: hlist.ToTraversable.Aux[KeysRepr, List, Symbol],
-      configuration: Configuration,
-      context: SerdesContext,
-      decoderEvidence: Lazy[ConfiguredDecoder[T]]
-    ): Decoder[T] = {
-      lazy val expectedFields = Keys[Repr]
-        .apply()
-        .toList
-        .map(fieldSymbol => configuration.transformMemberNames(fieldSymbol.name))
-        .toSet
-      lazy val predicate = { c: HCursor =>
-        val maybeOk = for {
-          json <- c.focus
-          jsonKeys <- json.hcursor.keys
-        } yield jsonKeys.toSet == expectedFields
-        maybeOk.getOrElse(false)
-      }
-      if(context.strictDeser)
-        origDeriveDecoder[T].validate(predicate, s"Unexpected fields. Valid fields: ${expectedFields.mkString(",")}.")
-      else
-        origDeriveDecoder[T]
-    }
 
     implicit lazy val aDecoder: Decoder[A] = deriveDecoder
   }
---
+++ src/main/scala/org/pfcoperez/custom/circe/semiauto.scala
@@ -0,0 +1,42 @@
+package org.pfcoperez.custom.circe
+
+import io.circe.{Decoder, HCursor}
+import io.circe.generic.extras.Configuration
+import io.circe.generic.extras.decoding.ConfiguredDecoder
+import org.pfcoperez.SerdesContext
+import shapeless.{HList, LabelledGeneric, Lazy}
+import shapeless.ops.hlist
+import shapeless.ops.record.Keys
+
+import io.circe.generic.extras.semiauto.{deriveDecoder => origDeriveDecoder}
+
+object semiauto {
+
+  def deriveDecoder[T, Repr <: HList, KeysRepr <: HList](
+    implicit
+    lgen: LabelledGeneric.Aux[T, Repr],
+    keys: Keys.Aux[Repr, KeysRepr],
+    toTraversable: hlist.ToTraversable.Aux[KeysRepr, List, Symbol],
+    configuration: Configuration,
+    context: SerdesContext,
+    decoderEvidence: Lazy[ConfiguredDecoder[T]]
+  ): Decoder[T] = {
+    lazy val expectedFields = Keys[Repr]
+      .apply()
+      .toList
+      .map(fieldSymbol => configuration.transformMemberNames(fieldSymbol.name))
+      .toSet
+    lazy val predicate = { c: HCursor =>
+      val maybeOk = for {
+        json <- c.focus
+        jsonKeys <- json.hcursor.keys
+      } yield jsonKeys.toSet == expectedFields
+      maybeOk.getOrElse(false)
+    }
+    if(context.strictDeser)
+      origDeriveDecoder[T].validate(predicate, s"Unexpected fields. Valid fields: ${expectedFields.mkString(",")}.")
+    else
+      origDeriveDecoder[T]
+  }
+
+}
